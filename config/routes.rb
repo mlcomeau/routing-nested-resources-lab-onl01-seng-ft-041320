3ba602ea143b85cc7b0bee8be4b9bb23b2d6a3ee
Megan Comeau <mlcomeau99@gmail.com>
2020-06-23 00:48:07 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:show] do
-    resources :songs, only: [:show, :index]
+  resources :artists do
+    resources :songs, only: [:index, :show]
   end
-  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+  resources :songs
 end
